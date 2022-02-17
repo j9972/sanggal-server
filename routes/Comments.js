6b29edde8d13d38efff87c1b517ff20f2d6fb267
jung su young <j9972@naver.com>
2022-02-17 22:02:24 +0900

    connect post route with a DB by checking postman

--- routes/Comments.js
+++ routes/Comments.js
@@ -3,4 +3,15 @@ const router = express.Router();
 
 const { Comments } = require("../models");
 
+router.get("/", async (req, res) => {
+  const listOfComment = await Comments.findAll();
+  res.json(listOfComment);
+});
+
+router.post("/", async (req, res) => {
+  const comment = req.body;
+  await Comments.create(comment);
+  res.json(comment);
+});
+
 module.exports = router;
